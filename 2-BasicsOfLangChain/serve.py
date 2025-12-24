ca9118d2a495b42f972e46e7fb178952cf44ce3c
Sandeep Hidellarachchi <sandeeph.de96@gmail.com>
2025-12-24 22:26:08 +0100

    Build basic LLM apps with LCEL and LangServe

---
+++ 2-BasicsOfLangChain/serve.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+from langserve import add_routes
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+groq_api_key = os.getenv('GROQ_API_KEY')
+model = ChatGroq(model='llama-3.1-8b-instant', groq_api_key=groq_api_key)
+
+
+system_template = 'Translate the following into {language}'
+
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+     ('system',system_template),
+     ('user','{text}')
+    ]
+)
+
+
+parser = StrOutputParser()
+
+# Create chain
+
+chain = prompt_template|model|parser
+
+# App definition
+app = FastAPI(title='Langchain server',
+              version=1.2,
+              description='sinple langchain api server with langchain runnable itnterface')
+
+
+add_routes(
+    app,
+    chain,
+    path="/chain"
+)
+
+
+if __name__=="__main__":
+    import uvicorn
+    uvicorn.run(app, host='127.0.0.1', port=8000)
